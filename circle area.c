9602d022de63ed8633601ec114ebcc868e6a07af
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2023-12-17 23:11:32 +0530

    circle area

---
+++ circle area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14
+int main()
+{
+    float r, area;
+    printf("Enter the radius:");
+    scanf("%f", &r);
+    area = PI * r * r;
+
+    printf("Area of circle is:%f\n", r);
+
+    return 0;
+}
